ecb012818d376c6f9241388810ec9775c506deb7
CodingMakoto <antoine@courant.pro>
2024-07-27 22:58:07 +0200

    Refacto fetch.py & updated database auth method

--- sql/fetch.py
+++ sql/fetch.py
@@ -1,6 +1,6 @@
 """
 The fetch file is providing commands to
-fetch/update/insert user informations
+fetch/update/insert user information
 of/into Raijin database.
 
 Commands:
@@ -8,31 +8,39 @@ Commands:
     - getLang
 """
 
-
 import discord
 import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv(dotenv_path='../')
+db_host = os.getenv("DB_HOST")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_database = os.getenv("DB_DATABASE")
+db_auth_plugin = os.getenv("DB_AUTH_PLUGIN")
 
 
 class Fetch(discord.Cog):
     """
-    Fetch class purpose is to have all the usefull queries
+    Fetch class purpose is to have all the useful queries
     in a single file to save lines on others
     """
+
     def __init__(self, bot):
         self.bot = bot
         self.mydb = mysql.connector.connect(
-            host="",
-            user="",
-            password="",
-            database="",
-            auth_plugin="",
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_database,
+            auth_plugin=db_auth_plugin,
         )
         self.cursor = self.mydb.cursor()
 
-
     def getaccount(self, member: int, guild: int):
         """
-        Look for an existing account with the informations gived,
+        Look for an existing account with the information given,
         return None if not existing
         """
         self.cursor.execute(
@@ -43,10 +51,9 @@ class Fetch(discord.Cog):
             return True
         return None
 
-
     def getlang(self, member: int, guild: int):
         """
-        Look for an existing lang with the informations gived,
+        Look for an existing language with the information given,
         return None if not existing
         """
         lang = ""
