d7d06e0b7d72cea027bca2e60af9af5ff6cd9868
Antoine <89703992+CodingMakoto@users.noreply.github.com>
2024-09-21 22:30:09 +0200

    Added main filed & README

---
+++ README.md
@@ -0,0 +1,85 @@
+<a name="readme-top"></a>
+
+
+
+<br />
+<div align="center">
+   <img src="res/images/avatar-wb.png" alt="Logo" width="220" height="220">
+
+  <h3 align="center">Raijin Discord Bot</h3>
+
+  <p align="center">
+    Have you ever wanted to live an New Inazuma Adventure on Discord ?
+    <br />
+    An Unofficial Genshin Impact Discord Bot by Makoto#7116
+    <br />
+    <!--<a href="https://raidenshogun.gitbook.io/docs"><strong>Documentation</strong></a>--><a href="https://discord.gg/2AePNcphrs"><strong>Discord Server</strong></a>
+    <br />
+    <br />
+  </p>
+</div>
+
+
+
+
+## Discover Raijin
+
+So... Hello ! I'm Raijin Discord Bot ! My goal was to create a New Adventure on Discord based on the Genshin Impact Game. I can fetch your Personal Info in the real game also, with me you are going to discover the New Adventure with a new guide named : Makoto and visit a New Island inspired of Genshin Impact.
+- Complete quests and chapters
+- View your daily quests
+- Heal yourself by using statues of the seven
+- Meet bosses and fight them
+- Collect materials and earn characters to be more powerful in battle !
+
+<p align="center">
+  <img src="res/images/story/1-english.jpg"/>
+</p>
+
+
+
+
+## Good to know
+
+⚠️ **STILL IN DEVELOPMENT, NOT FINISHED YET**
+
+⚠️ Stable version are only RELEASES files and not those of the main branch
+
+⚠️ DISCLAIMER : I'm a Student Developer that create this by himself. My code work but can be very different from many advanced bots
+
+
+
+
+## Supported languages
+
+🇫🇷 **Français**
+
+🇬🇧 **English**
+
+
+
+
+## Languages used
+
+[![Made withJupyter](https://img.shields.io/badge/Made%20with-Python-yellow?style=for-the-badge&logo=Python)](https://python.org/)
+
+Using :
+
+[![Json][JSON]][JSON-url]
+[![Mysql][MySQL]][MySQL-url]
+
+
+
+
+
+## Contact
+
+For any questions we invite you to join us on the [Discord Server](https://discord.gg/2AePNcphrs).
+
+Also you can post your issue if you have one on this repository issue page.
+
+
+[JSON]: https://img.shields.io/badge/Json-f7df1e?style=for-the-badge&logo=json&logoColor=383838
+[JSON-url]: https://json.org/
+
+[MySQL]: https://img.shields.io/badge/MySQL-005B75?style=for-the-badge&logo=mysql&logoColor=F3A01F
+[MySQL-url]: https://www.mysql.com/
---
+++ main.py
@@ -0,0 +1,92 @@
+"""
+The main file is providing events for guild,
+commands and connection.
+
+Events:
+    - on_guild_join
+    - on_application_command_error
+    - on_connect
+    - on_ready
+"""
+
+
+import time
+import json
+import os
+from dotenv import load_dotenv
+import discord
+
+
+load_dotenv()
+raijin_token = os.getenv("BOT_TOKEN")
+intents = discord.Intents.all()
+bot = discord.Bot(intents=intents)
+DEFAULT_COLOR = 0x37266A
+
+
+@bot.event
+async def on_guild_join(guild):
+    """
+    Send a message when Raijin is joining a guild
+    """
+    file = discord.File(
+        "res/images/avatar-wb.png", filename="avatar.png"
+    )
+    en_file = open('res/lang/en_EN.json', encoding="utf-8")
+    messages = json.load(en_file)
+    title = "<:raidenbird:1080897824440455288>  Raijin Discord Bot"
+    welcoming = discord.Embed(
+        title=title,
+        description=messages['welcome'],
+        color=DEFAULT_COLOR,
+    )
+    welcoming.set_thumbnail(url="attachment://avatar.png")
+    welcoming.set_footer(text="Created and Developed by Makoto#7116")
+
+    channel = discord.utils.get(guild.text_channels)
+    if channel.permissions_for(guild.me).send_messages:
+        await channel.send(embed=welcoming, file=file)
+    else:
+        await guild.owner.send(embed=welcoming, file=file)
+
+
+@bot.event
+async def on_application_command_error(ctx: discord.ApplicationContext,
+    error: discord.DiscordException):
+    """
+    Send a message when an error appear on command call/execution
+    """
+    if isinstance(error, discord.ApplicationCommandError):
+        local = time.localtime()
+        current_time = time.strftime("%H:%M:%S", local)
+        errors = discord.Embed(
+            title="Error while executing the command",
+            description=f"Please report this error below in our discord\
+            server <:raidencry:1189307566606520501>\n`{error}`",
+            color=0xFF0000,
+        )
+        errors.set_footer(text=f"Detected at {current_time} UTC")
+        await ctx.respond(embed=errors)
+        print("Error detected:", error)
+
+
+@bot.event
+async def on_connect():
+    """
+    Register new commands and sync modified commands with the API
+    """
+    await bot.sync_commands()
+
+
+@bot.event
+async def on_ready():
+    """
+    Send a message when the bot is ready
+    """
+    print("Raijin is Ready")
+
+
+bot.load_extension("src.adventure.account")
+
+
+bot.run(raijin_token)
