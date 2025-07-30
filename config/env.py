028dd550bd910b63567773ec029fdfa71dd7b7a2
CodingMakoto <antoine@courant.pro>
2025-07-30 12:39:46 +0200

    REMOVE Class docstring

--- config/env.py
+++ config/env.py
@@ -3,13 +3,8 @@ import os
 import mysql.connector
 from dotenv import load_dotenv
 
+
 class Database:
-    """
-    Database is made to create a connection with
-    the database and return the connection if
-    successfull to get the needed user data and
-    to close a connection
-    """
     def __init__(self):
         self.conn = self.connect()
 
@@ -20,7 +15,7 @@ class Database:
             user=os.getenv("DB_USER"),
             password=os.getenv("DB_PASSWORD"),
             database=os.getenv("DB_DATABASE"),
-            auth_plugin=os.getenv("DB_AUTH_PLUGIN")
+            auth_plugin=os.getenv("DB_AUTH_PLUGIN"),
         )
 
     def close(self):
